a2e1549da41fe56c817a5ccb4b52c5b9ca7cb124
Byunk <clearman001@gmail.com>
2023-05-22 17:36:31 +0900

    chore: add eslint-disable for any

--- src/app/controllers/channel.controller.ts
+++ src/app/controllers/channel.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
 import { Channel } from '../schemas';
 import logger from '../util/logger';
